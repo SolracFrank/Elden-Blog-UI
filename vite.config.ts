852209640abe93b09eaaf9edb7c0c89b7c8b3eb4
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-11-13 13:10:27 -0600

    feat: configure https for development
    HTTPS has been configured for development (localhost)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
+
+const key = fs.readFileSync(path.join(__dirname, './secrets/localhost-key.pem'), 'utf-8');
+const cert = fs.readFileSync(path.join(__dirname, './secrets/localhost.pem'), 'utf-8');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key,
+      cert,
+    },
+  },
 })
