937432f7538cb77cc8b355d3821e4ea0e6af825a
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-11 13:11:51 -0600

    add: auth cookie util

---
+++ src/utils/authCookieSetter.ts
@@ -0,0 +1,58 @@
+import { UserCookies } from "../interfaces/authInterfaces";
+import Cookies from "universal-cookie";
+
+
+export const setUserCookies = ({
+    cookies,
+    jwtExpires,
+    userId,
+    userName,
+    email,
+    jwToken,
+  }: UserCookies) => {
+  
+    cookies.set("userId", userId, {
+    });
+    cookies.set("userName", userName, {
+    });
+    cookies.set("email", email, {
+    });
+    cookies.set("JWToken", jwToken, { 
+      expires: new Date(jwtExpires),
+    });
+    cookies.set("JWTokenExpires", jwtExpires, {
+      expires: new Date(jwtExpires),
+    });
+  };
+
+  export const removeUserCookies = (cookies: Cookies) => {
+    cookies.remove("userId");
+    cookies.remove("userName");
+    cookies.remove("JWToken");
+    cookies.remove("JWTokenExpires");
+    cookies.remove("RefreshToken");
+    cookies.remove("RefreshTokenExpires");
+    cookies.remove("todoUser");
+  };
+
+  export const getUserCookies = (cookies: Cookies) => {
+    const userName = cookies.get("userName");
+    const email = cookies.get("email");
+    const userId = cookies.get("userId");
+    const jwToken = cookies.get("JWToken");
+  
+  
+    const jwtExpiresRaw = cookies.get("JWTokenExpires");
+    const jwtExpiresDecoded = jwtExpiresRaw ? decodeURIComponent(jwtExpiresRaw) : "";
+  
+
+  
+    return {
+      userName,
+      userId,
+      email,
+      jwToken,
+      jwtExpires: jwtExpiresDecoded,
+    };
+  };
+  
