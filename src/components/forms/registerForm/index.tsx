2d8fd8b8f7ca060f020fbbc951157e587835ca46
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-18 13:06:42 -0600

    feat: refactor register form

--- src/components/forms/registerForm/index.tsx
+++ src/components/forms/registerForm/index.tsx
@@ -3,9 +3,18 @@ import { registerProps } from "../../../interfaces/authInterfaces";
 import registerSchema from "../../../schemas/userSchemas/registerSchema";
 import { DateConverter } from "../../../utils/dateConverter";
 import { useState } from "react";
+import { UseRegister } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
+import { ProblemDetails } from "../../../interfaces/httpInterfaces";
+import toast from "react-hot-toast";
 
 const RegisterForm = () => {
   const [zodErrors, setZodErros] = useState<Record<string, string>>({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { registerUser } = UseRegister();
+  const navigate = useNavigate();
 
   const ValidateOnSubmit = (
     values: registerProps,
@@ -13,7 +22,7 @@ const RegisterForm = () => {
   ) => {
     const newValuest = { ...values, birthdate: DateConverter(values.birthday) };
     const result = registerSchema.safeParse(newValuest);
-    
+
     if (!result.success) {
       const errors: Record<string, string> = {};
       result.error.issues.forEach((error) => {
@@ -21,15 +30,14 @@ const RegisterForm = () => {
 
         setZodErros(errors);
         setErrors(errors);
-        console.log('errors: ', errors); //101 TO DELETE
+        console.log("errors: ", errors); //101 TO DELETE
       });
       return false;
     }
     return true;
-
   };
 
-  const { handleSubmit, handleChange, setErrors} = useFormik({
+  const { handleSubmit, handleChange, setErrors } = useFormik({
     initialValues: {
       email: "",
       username: "",
@@ -37,10 +45,30 @@ const RegisterForm = () => {
       password: "",
       repeatpassword: "",
     },
-    onSubmit: (values: registerProps) => {
-      const isValid = ValidateOnSubmit(values, setErrors);
-      if (isValid) {
-        console.log("Sending data to API: ", values); // 101 TO DELETE
+    onSubmit: async (values: registerProps) => {
+      const isValid = await ValidateOnSubmit(values, setErrors);
+      if (!isValid) {
+        console.log("Invalid inputs, ERRORS: ", values); // 101 TO DELETE
+        return;
+      }
+      console.log("Correct Values: ", values); // 101 TO DELETE
+      console.log("Sending to API");
+      
+      setIsLoading(true);
+      try {
+        const response = await registerUser(values);
+        console.log('api response: ',response); // 101 TO DELETE
+        
+        if (response.status === 200 || response.status === 201) {
+          navigate("/login");
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        const problemDetails = axiosError.response?.data as ProblemDetails;
+        const { title, errors } = problemDetails || {};
+        toast.error(`${title} ${errors}`);
+      } finally {
+        setIsLoading(false);
       }
     },
     validateOnChange: false,
@@ -64,7 +92,9 @@ const RegisterForm = () => {
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-            {zodErrors.username && <div className="text-red-600">{zodErrors.username}</div>}
+          {zodErrors.username && (
+            <div className="text-red-600">{zodErrors.username}</div>
+          )}
         </div>
         <div className="mb-4">
           <label
@@ -80,8 +110,9 @@ const RegisterForm = () => {
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-            {zodErrors.birthday && <div className="text-red-600">{zodErrors.birthday}</div>}
-
+          {zodErrors.birthday && (
+            <div className="text-red-600">{zodErrors.birthday}</div>
+          )}
         </div>
         <div className="mb-4">
           <label
@@ -98,8 +129,9 @@ const RegisterForm = () => {
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-            {zodErrors.email && <div className="text-red-600">{zodErrors.email}</div>}
-
+          {zodErrors.email && (
+            <div className="text-red-600">{zodErrors.email}</div>
+          )}
         </div>
         <div className="mb-4">
           <label
@@ -115,8 +147,9 @@ const RegisterForm = () => {
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-            {zodErrors.password && <div className="text-red-600">{zodErrors.password}</div>}
-
+          {zodErrors.password && (
+            <div className="text-red-600">{zodErrors.password}</div>
+          )}
         </div>
         <div className="mb-4">
           <label
@@ -132,13 +165,17 @@ const RegisterForm = () => {
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-            {zodErrors.repeatpassword && <div className="text-red-600">{zodErrors.repeatpassword}</div>}
-          
+          {zodErrors.repeatpassword && (
+            <div className="text-red-600">{zodErrors.repeatpassword}</div>
+          )}
         </div>
         <div className="flex items-center justify-between mt-8">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
+            disabled={isLoading}
+            value={isLoading ? "SIGNING" : "SIGN UP"}
+
           >
             Submit
           </button>
