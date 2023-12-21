94bb4f4b4884d470076f16bbf1a5a259b05ada23
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-21 00:42:44 -0600

    feat: wrap App with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <AuthProvider>
+        <Toaster/>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </>
+  );
 };
 export default App;
