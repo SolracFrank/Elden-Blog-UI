4e342f6a2c403d377dc586f090bce83e6bc04532
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-21 00:41:45 -0600

    add: login functionality to useAuth

--- src/components/hooks/useAuth.tsx
+++ src/components/hooks/useAuth.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
-import { registerProps } from "../../interfaces/authInterfaces";
-import { registerService } from "../../services/authService";
+import { loginProps, registerProps } from "../../interfaces/authInterfaces";
+import { loginService, registerService } from "../../services/authService";
 
 export const UseRegister = () => {
   const registerUser = useCallback(async (data: registerProps) => {
@@ -8,4 +8,13 @@ export const UseRegister = () => {
     return response;
   }, []);
   return { registerUser };
+};
+
+export const UseLogin = () => {
+  const loginUser = useCallback(async (data: loginProps) => {
+    const response = await loginService(data);
+    return response;
+  }, []);
+
+  return { loginUser };
 };
