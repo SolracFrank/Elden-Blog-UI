e0713405e3e231ab62c457ee179ffbbcd143a471
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-21 00:42:20 -0600

    feat: improve login interface name

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,7 @@
-import { Login, registerProps } from "../interfaces/authInterfaces.ts";
+import { loginProps, registerProps } from "../interfaces/authInterfaces.ts";
 import api from "./axiosService.ts";
 
-export const loginService = async (loginData: Login) => {
+export const loginService = async (loginData: loginProps) => {
   const response = await api.post("/v1/Auth/signin", loginData);
   return response;
 };
