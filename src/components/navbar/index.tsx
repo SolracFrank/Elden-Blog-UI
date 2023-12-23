7e628fd1fbef5c2f1e320fe25a78894865b38eb3
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-23 13:01:18 -0600

    feat: sessioned navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,18 +1,38 @@
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../../context/useAuthContext";
 
 const NavBar = () => {
-    return(
-        <nav className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-            <NavLink to="/" className="text-xl font-semibold">Elden Blogs</NavLink>
-            <ul className="flex space-x-4">
-                <NavLink to='/login' className="hover:text-gray-300">Log in</NavLink>
-                <NavLink to='/register' className="hover:text-gray-300">Join</NavLink>
-            </ul>
-        </div>
+  const { isAuthenticated, userName, logout} = useAuthContext();
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <NavLink to="/" className="text-xl font-semibold">
+          Elden Blogs
+        </NavLink>
+        <ul className="flex space-x-4">
+          {!isAuthenticated ? (
+            <>
+              <NavLink to="/login" className="hover:text-gray-300">
+                Log in
+              </NavLink>
+              <NavLink to="/register" className="hover:text-gray-300">
+                Join
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to="/" className="hover:text-gray-300">
+                {userName}
+              </NavLink>
+              <button onClick={logout} className="hover:text-gray-300">
+                Logout
+              </button>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
-    
-    );
-}
+  );
+};
 
 export default NavBar;
